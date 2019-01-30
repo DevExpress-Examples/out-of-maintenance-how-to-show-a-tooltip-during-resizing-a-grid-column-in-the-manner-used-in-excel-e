41eda147226798b6e4fb0af27b546993df935983
Norikatel <igor.kurzaev@devexpress.com>
2019-01-30 12:07:13 +0300

    update example

--- VB/WindowsApplication3/Form1.vb
+++ VB/WindowsApplication3/Form1.vb
@@ -1,38 +1,28 @@
 Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.ComponentModel
-Imports System.Data
-Imports System.Drawing
-Imports System.Text
-Imports System.Windows.Forms
-Imports DevExpress.XtraEditors
 Imports DevExpress.Utils
-Imports DevExpress.XtraGrid.Views.Grid
-Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
-Imports DevExpress.XtraGrid.Views.Grid.Drawing
+Imports DevExpress.XtraEditors
 Imports DevExpress.XtraGrid.Columns
 Imports DevExpress.XtraGrid.Drawing
-
+Imports DevExpress.XtraGrid.Views.Grid
+Imports DevExpress.XtraGrid.Views.Grid.Drawing
+Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
+Imports System.ComponentModel
 
 Namespace DXSample
 	Partial Public Class Form1
 		Inherits XtraForm
 		Public Sub New()
 			InitializeComponent()
+			Dim list = New BindingList(Of Item)()
+			For i As Integer = 0 To 19
+				list.Add(New Item() With {.ID = i, .Name = "TestName" & i, .Description = "TestDescription" & i})
+			Next i
+			gridControl1.DataSource = list
 		End Sub
-
-
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			' TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
-			Me.productsTableAdapter.Fill(Me.nwindDataSet.Products)
-		End Sub
-
 		Private Sub OnGetActiveObjectInfo(ByVal sender As Object, ByVal e As DevExpress.Utils.ToolTipControllerGetActiveObjectInfoEventArgs) Handles toolTipController1.GetActiveObjectInfo
 			If e.SelectedControl IsNot gridControl1 Then
 				Return
 			End If
-
 			Dim info As ToolTipControlInfo = Nothing
 			Dim view As GridView = TryCast(gridControl1.GetViewAt(e.ControlMousePosition), GridView)
 			If view Is Nothing Then
@@ -51,7 +41,6 @@ Namespace DXSample
 				e.Info = info
 			End If
 		End Sub
-
 		Protected Function GetColumnHeaderWidth(ByVal args As GridColumnInfoArgs, ByVal painter As GridPainter) As Integer
 			If args IsNot Nothing Then
 				Dim width As Integer = painter.CurrentSizerPos - args.Bounds.Left
@@ -62,5 +51,34 @@ Namespace DXSample
 			End If
 			Return 0
 		End Function
+	End Class
+	Public Class Item
+		Private privateID As Integer
+		Public Property ID() As Integer
+			Get
+				Return privateID
+			End Get
+			Set(ByVal value As Integer)
+				privateID = value
+			End Set
+		End Property
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+		Private privateDescription As String
+		Public Property Description() As String
+			Get
+				Return privateDescription
+			End Get
+			Set(ByVal value As String)
+				privateDescription = value
+			End Set
+		End Property
 	End Class
 End Namespace
