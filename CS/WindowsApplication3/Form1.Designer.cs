e7b02ca1d6a49a2a8e84f21e0367c705c07994ea
Norikatel <igor.kurzaev@devexpress.com>
2019-01-30 11:50:46 +0300

    update example

--- CS/WindowsApplication3/Form1.Designer.cs
+++ CS/WindowsApplication3/Form1.Designer.cs
@@ -26,20 +26,9 @@ namespace DXSample {
             this.components = new System.ComponentModel.Container();
             this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
-            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.nwindDataSet = new DXSample.nwindDataSet();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.colProductID = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colProductName = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colSupplierID = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colCategoryID = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colQuantityPerUnit = new DevExpress.XtraGrid.Columns.GridColumn();
-            this.colUnitPrice = new DevExpress.XtraGrid.Columns.GridColumn();
             this.toolTipController1 = new DevExpress.Utils.ToolTipController(this.components);
-            this.productsTableAdapter = new DXSample.nwindDataSetTableAdapters.ProductsTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,101 +38,38 @@ namespace DXSample {
             // 
             // gridControl1
             // 
-            this.gridControl1.DataSource = this.productsBindingSource;
             this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gridControl1.Location = new System.Drawing.Point(0, 0);
             this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(870, 534);
+            this.gridControl1.Size = new System.Drawing.Size(746, 434);
             this.gridControl1.TabIndex = 0;
             this.gridControl1.ToolTipController = this.toolTipController1;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
-            // productsBindingSource
-            // 
-            this.productsBindingSource.DataMember = "Products";
-            this.productsBindingSource.DataSource = this.nwindDataSet;
-            // 
-            // nwindDataSet
-            // 
-            this.nwindDataSet.DataSetName = "nwindDataSet";
-            this.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
             // gridView1
             // 
-            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
-            this.colProductID,
-            this.colProductName,
-            this.colSupplierID,
-            this.colCategoryID,
-            this.colQuantityPerUnit,
-            this.colUnitPrice});
+            this.gridView1.DetailHeight = 284;
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
-            // 
-            // colProductID
-            // 
-            this.colProductID.FieldName = "ProductID";
-            this.colProductID.Name = "colProductID";
-            this.colProductID.Visible = true;
-            this.colProductID.VisibleIndex = 0;
-            // 
-            // colProductName
-            // 
-            this.colProductName.FieldName = "ProductName";
-            this.colProductName.Name = "colProductName";
-            this.colProductName.Visible = true;
-            this.colProductName.VisibleIndex = 1;
-            // 
-            // colSupplierID
-            // 
-            this.colSupplierID.FieldName = "SupplierID";
-            this.colSupplierID.Name = "colSupplierID";
-            this.colSupplierID.Visible = true;
-            this.colSupplierID.VisibleIndex = 2;
-            // 
-            // colCategoryID
-            // 
-            this.colCategoryID.FieldName = "CategoryID";
-            this.colCategoryID.Name = "colCategoryID";
-            this.colCategoryID.Visible = true;
-            this.colCategoryID.VisibleIndex = 3;
-            // 
-            // colQuantityPerUnit
-            // 
-            this.colQuantityPerUnit.FieldName = "QuantityPerUnit";
-            this.colQuantityPerUnit.Name = "colQuantityPerUnit";
-            this.colQuantityPerUnit.Visible = true;
-            this.colQuantityPerUnit.VisibleIndex = 4;
-            // 
-            // colUnitPrice
-            // 
-            this.colUnitPrice.FieldName = "UnitPrice";
-            this.colUnitPrice.Name = "colUnitPrice";
-            this.colUnitPrice.Visible = true;
-            this.colUnitPrice.VisibleIndex = 5;
             // 
             // toolTipController1
             // 
             this.toolTipController1.GetActiveObjectInfo += new DevExpress.Utils.ToolTipControllerGetActiveObjectInfoEventHandler(this.OnGetActiveObjectInfo);
             // 
-            // productsTableAdapter
-            // 
-            this.productsTableAdapter.ClearBeforeFill = true;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(870, 534);
+            this.ClientSize = new System.Drawing.Size(746, 434);
             this.Controls.Add(this.gridControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "How to show a tooltip during resizing a grid column";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nwindDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -154,15 +80,6 @@ namespace DXSample {
         private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
-        private nwindDataSet nwindDataSet;
-        private System.Windows.Forms.BindingSource productsBindingSource;
-        private DXSample.nwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
-        private DevExpress.XtraGrid.Columns.GridColumn colProductID;
-        private DevExpress.XtraGrid.Columns.GridColumn colProductName;
-        private DevExpress.XtraGrid.Columns.GridColumn colSupplierID;
-        private DevExpress.XtraGrid.Columns.GridColumn colCategoryID;
-        private DevExpress.XtraGrid.Columns.GridColumn colQuantityPerUnit;
-        private DevExpress.XtraGrid.Columns.GridColumn colUnitPrice;
         private DevExpress.Utils.ToolTipController toolTipController1;
     }
 }
--- VB/WindowsApplication3/Form1.Designer.vb
+++ VB/WindowsApplication3/Form1.Designer.vb
@@ -28,20 +28,9 @@ Namespace DXSample
 			Me.components = New System.ComponentModel.Container()
 			Me.defaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
 			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.productsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-			Me.nwindDataSet = New DXSample.nwindDataSet()
 			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.colProductID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colProductName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colSupplierID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colCategoryID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colQuantityPerUnit = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colUnitPrice = New DevExpress.XtraGrid.Columns.GridColumn()
 			Me.toolTipController1 = New DevExpress.Utils.ToolTipController(Me.components)
-			Me.productsTableAdapter = New DXSample.nwindDataSetTableAdapters.ProductsTableAdapter()
 			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.productsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
 			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
 			Me.SuspendLayout()
 			' 
@@ -51,94 +40,37 @@ Namespace DXSample
 			' 
 			' gridControl1
 			' 
-			Me.gridControl1.DataSource = Me.productsBindingSource
 			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.gridControl1.EmbeddedNavigator.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
 			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
 			Me.gridControl1.MainView = Me.gridView1
+			Me.gridControl1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
 			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(870, 534)
+			Me.gridControl1.Size = New System.Drawing.Size(746, 434)
 			Me.gridControl1.TabIndex = 0
 			Me.gridControl1.ToolTipController = Me.toolTipController1
 			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
 			' 
-			' productsBindingSource
-			' 
-			Me.productsBindingSource.DataMember = "Products"
-			Me.productsBindingSource.DataSource = Me.nwindDataSet
-			' 
-			' nwindDataSet
-			' 
-			Me.nwindDataSet.DataSetName = "nwindDataSet"
-			Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-			' 
 			' gridView1
 			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colProductID, Me.colProductName, Me.colSupplierID, Me.colCategoryID, Me.colQuantityPerUnit, Me.colUnitPrice})
+			Me.gridView1.DetailHeight = 284
 			Me.gridView1.GridControl = Me.gridControl1
 			Me.gridView1.Name = "gridView1"
-			' 
-			' colProductID
-			' 
-			Me.colProductID.FieldName = "ProductID"
-			Me.colProductID.Name = "colProductID"
-			Me.colProductID.Visible = True
-			Me.colProductID.VisibleIndex = 0
-			' 
-			' colProductName
-			' 
-			Me.colProductName.FieldName = "ProductName"
-			Me.colProductName.Name = "colProductName"
-			Me.colProductName.Visible = True
-			Me.colProductName.VisibleIndex = 1
-			' 
-			' colSupplierID
-			' 
-			Me.colSupplierID.FieldName = "SupplierID"
-			Me.colSupplierID.Name = "colSupplierID"
-			Me.colSupplierID.Visible = True
-			Me.colSupplierID.VisibleIndex = 2
-			' 
-			' colCategoryID
-			' 
-			Me.colCategoryID.FieldName = "CategoryID"
-			Me.colCategoryID.Name = "colCategoryID"
-			Me.colCategoryID.Visible = True
-			Me.colCategoryID.VisibleIndex = 3
-			' 
-			' colQuantityPerUnit
-			' 
-			Me.colQuantityPerUnit.FieldName = "QuantityPerUnit"
-			Me.colQuantityPerUnit.Name = "colQuantityPerUnit"
-			Me.colQuantityPerUnit.Visible = True
-			Me.colQuantityPerUnit.VisibleIndex = 4
-			' 
-			' colUnitPrice
-			' 
-			Me.colUnitPrice.FieldName = "UnitPrice"
-			Me.colUnitPrice.Name = "colUnitPrice"
-			Me.colUnitPrice.Visible = True
-			Me.colUnitPrice.VisibleIndex = 5
 			' 
 			' toolTipController1
 			' 
 '			Me.toolTipController1.GetActiveObjectInfo += New DevExpress.Utils.ToolTipControllerGetActiveObjectInfoEventHandler(Me.OnGetActiveObjectInfo);
 			' 
-			' productsTableAdapter
-			' 
-			Me.productsTableAdapter.ClearBeforeFill = True
-			' 
 			' Form1
 			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(7F, 16F)
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
 			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(870, 534)
+			Me.ClientSize = New System.Drawing.Size(746, 434)
 			Me.Controls.Add(Me.gridControl1)
+			Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
 			Me.Name = "Form1"
 			Me.Text = "How to show a tooltip during resizing a grid column"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
 			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.productsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).EndInit()
 			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
 			Me.ResumeLayout(False)
 
@@ -149,15 +81,6 @@ Namespace DXSample
 		Private defaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
 		Private gridControl1 As DevExpress.XtraGrid.GridControl
 		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private nwindDataSet As nwindDataSet
-		Private productsBindingSource As System.Windows.Forms.BindingSource
-		Private productsTableAdapter As DXSample.nwindDataSetTableAdapters.ProductsTableAdapter
-		Private colProductID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colProductName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colSupplierID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colCategoryID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colQuantityPerUnit As DevExpress.XtraGrid.Columns.GridColumn
-		Private colUnitPrice As DevExpress.XtraGrid.Columns.GridColumn
 		Private WithEvents toolTipController1 As DevExpress.Utils.ToolTipController
 	End Class
 End Namespace
